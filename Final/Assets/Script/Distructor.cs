4cf088871f05fd0e865c0530616d5f59550769ca
Brian12542 <assassin12542@gmail.com>
2022-05-31 12:51:56 +0700

    31.5.2565.12.51

--- Final/Assets/Script/Distructor.cs
+++ Final/Assets/Script/Distructor.cs
@@ -17,7 +17,7 @@ public class Distructor : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
-        if (collisionInfo.collider.tag == "Player")
+        if (collisionInfo.collider.tag == "ship")
         {
             Destroy(gameObject);
         }
